755c8db06372538f021eb9eb8b0811b83ffcd1c7
Ben Stuart <bentstuart@gmail.com>
2018-05-14 12:26:22 +0930

    comments added, visual graph representation of learned linear line.

--- linear_support_vector_classifier.py
+++ linear_support_vector_classifier.py
@@ -37,7 +37,7 @@ X = np.array([[1,2],
 
 # Our "targets" or "labels"
 # Here I define 1 to mean stock solved  above recommended stock price and 0 to mean sold below stock price
-# I am labeling each array plotting with what it's outcome was i.e what it means in the real world.
+# Here I am setting the target (what I expect the resulting values to be)
 y = [0,1,0,1,0,1]
 
 # C is a valuation of "how badly" you want to properly classify, or fit, everything. (1) is the default
@@ -46,7 +46,26 @@ clf = svm.SVC(kernel='linear', C = 1.0)
 
 # Sklearn required the data to be re-shaped for fitting
 reshaped_X = X.reshape(6, -1)
+
+# Give sklearn the data and the data's expected results
 clf.fit(X,y)
 
+# given this array would it be below or above given what sklearn(clf function) has learned
 print(clf.predict([0.58,0.76]))
+
+# given this array would it be below or above given what sklearn(clf function) has learned
 print(clf.predict([10.58,10.76]))
+
+w = clf.coef_[0]
+print(w)
+
+a = -w[0] / w[1]
+
+xx = np.linspace(0,12)
+yy = a * xx - clf.intercept_[0] / w[1]
+
+h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+
+plt.scatter(X[:, 0], X[:, 1], c = y)
+plt.legend()
+plt.show()
